b0aaa1aa0d3c91e9babedc3456ca5d72868a852e
Dylan <dylanmills@rogers.com>
2021-02-05 14:29:44 -0500

    Set up all custom theme supports
    Custom color palete, block editors assets, Foundation.js, etc.

---
+++ app/public/wp-content/themes/hoop/assets/js/editor.js
@@ -0,0 +1,7 @@
+wp.blocks.registerBlockStyle( 'core/quote', {
+    name: 'fancy-quote',
+    label: 'Fancy Quote'
+} );
+// wp.domReady(()=>{
+//     wp.blocks.unregisterBlockStyle('core/quote','large');
+// })
--- app/public/wp-content/themes/hoop/functions.php
+++ app/public/wp-content/themes/hoop/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Hoop functions and definitions
  *
@@ -7,12 +8,12 @@
  * @package Hoop
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( 'hoop_setup' ) ) :
+if (!function_exists('hoop_setup')) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,17 +21,18 @@ if ( ! function_exists( 'hoop_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function hoop_setup() {
+	function hoop_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on Hoop, use a find and replace
 		 * to change 'hoop' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'hoop', get_template_directory() . '/languages' );
+		load_theme_textdomain('hoop', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -38,19 +40,19 @@ if ( ! function_exists( 'hoop_setup' ) ) :
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-primary' => esc_html__( 'Primary', 'hoop' ),
+				'menu-primary' => esc_html__('Primary', 'hoop'),
 			)
 		);
 
@@ -84,7 +86,7 @@ if ( ! function_exists( 'hoop_setup' ) ) :
 		);
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		/**
 		 * Add support for core custom logo.
@@ -100,9 +102,73 @@ if ( ! function_exists( 'hoop_setup' ) ) :
 				'flex-height' => true,
 			)
 		);
+		/**
+		 *  Add support for the default block styles.
+		 **/
+		add_theme_support('wp-block-styles');
+
+		/**
+		 *  Add support for wide alignment.
+		 **/
+		add_theme_support('align-wide');
+
+		/**
+		 *  Add support for color palette.
+		 **/
+
+		add_theme_support('editor-color-palette', array(
+			array(
+				'name' => esc_attr__('Green', 'themeLangDomain'),
+				'slug' => 'green',
+				'color' => '#274b40',
+			),			array(
+				'name' => esc_attr__('Orange', 'themeLangDomain'),
+				'slug' => 'orange',
+				'color' => '#f8bd3b',
+			),			array(
+				'name' => esc_attr__('Black', 'themeLangDomain'),
+				'slug' => 'black',
+				'color' => '#000000',
+			),			array(
+				'name' => esc_attr__('White', 'themeLangDomain'),
+				'slug' => 'white',
+				'color' => '#ffffff',
+			),
+		));
+
+		/**
+		 *  Add support for color gradients.
+		 **/
+		add_theme_support(
+			'editor-gradient-presets',
+			array()
+		);
+
+		/**
+		 *  Add support for font sizes.
+		 **/
+		add_theme_support('editor-font-sizes', array(
+			array(
+				'name' => esc_attr__('Small', 'themeLangDomain'),
+				'size' => 12,
+				'slug' => 'small'
+			)
+		));
+
+		/**
+		 *  Disable theme supports.
+		 **/
+		add_theme_support('disable-custom-font-sizes');
+		add_theme_support('disable-custom-colors');
+		add_theme_support('disable-custom-gradients');
+
+		/**
+		 *  Remove core block patterns.
+		 **/
+		remove_theme_support('core-block-patterns');
 	}
 endif;
-add_action( 'after_setup_theme', 'hoop_setup' );
+add_action('after_setup_theme', 'hoop_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,22 +177,24 @@ add_action( 'after_setup_theme', 'hoop_setup' );
  *
  * @global int $content_width
  */
-function hoop_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'hoop_content_width', 640 );
+function hoop_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('hoop_content_width', 640);
 }
-add_action( 'after_setup_theme', 'hoop_content_width', 0 );
+add_action('after_setup_theme', 'hoop_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function hoop_widgets_init() {
+function hoop_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'hoop' ),
+			'name'          => esc_html__('Sidebar', 'hoop'),
 			'id'            => 'sidebar',
-			'description'   => esc_html__( 'Add widgets here.', 'hoop' ),
+			'description'   => esc_html__('Add widgets here.', 'hoop'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -134,20 +202,24 @@ function hoop_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'hoop_widgets_init' );
+add_action('widgets_init', 'hoop_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function hoop_scripts() {
-	wp_enqueue_style( 'hoop-style', get_stylesheet_uri(), array(), _S_VERSION );
+function hoop_scripts()
+{
+	wp_enqueue_style('hoop-style', get_stylesheet_uri(), array(), _S_VERSION);
 
+	//* Foundation
+	wp_enqueue_style('foundation-style', get_template_directory_uri() . '/assets/css/vendor/foundation.css');
+	wp_enqueue_script('foundation-script', get_template_directory_uri() . '/assets/js/vendor/foundation.js',array(),6.6.3,true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'hoop_scripts' );
+add_action('wp_enqueue_scripts', 'hoop_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -169,10 +241,31 @@ require get_template_directory() . '/inc/template-functions.php';
  */
 require get_template_directory() . '/inc/customizer.php';
 
+
 /**
- * Load Jetpack compatibility file.
+ * Enqueuing block editor assets.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+function hoop_enqueue_block_editor_assets()
+{
+	wp_enqueue_script(
+		'editor-script',
+		get_template_directory_uri() . '/assets/js/editor.js',
+		array('wp-blocks', 'wp-dom-ready', 'wp-edit-post')
+	);
 }
 
+add_action('enqueue_block_editor_assets', 'hoop_enqueue_block_editor_assets');
+
+
+/**
+ * Enqueuing block assets.
+ */
+
+function hoop_enqueue_block_assets()
+{
+	wp_enqueue_style(
+		'blocks-style',
+		get_template_directory_uri() . '/assets/css/blocks.css'
+	);
+}
+add_action('enqueue_block_assets', 'hoop_enqueue_block_assets');
