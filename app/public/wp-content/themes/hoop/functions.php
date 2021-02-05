1baee76416d07b0e5711c86037648c88a9eca030
Dylan <dylanmills@rogers.com>
2021-02-05 15:44:32 -0500

    Added Image Assets and Content To Homepage
    Image assets, Home page, Menu

--- app/public/wp-content/themes/hoop/functions.php
+++ app/public/wp-content/themes/hoop/functions.php
@@ -213,7 +213,7 @@ function hoop_scripts()
 
 	//* Foundation
 	wp_enqueue_style('foundation-style', get_template_directory_uri() . '/assets/css/vendor/foundation.css');
-	wp_enqueue_script('foundation-script', get_template_directory_uri() . '/assets/js/vendor/foundation.js',array(),6.6.3,true);
+	wp_enqueue_script('foundation-script', get_template_directory_uri() . '/assets/js/vendor/foundation.js',array(),'6.6.3',true);
 
 	if (is_singular() && comments_open() && get_option('thread_comments')) {
 		wp_enqueue_script('comment-reply');
