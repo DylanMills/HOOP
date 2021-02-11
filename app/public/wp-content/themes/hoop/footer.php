e2c14452095a8f952b6addaa8b7694443dd14d4d
Dylan <dylanmills@rogers.com>
2021-02-11 17:52:31 -0500

    Created and Styled footer, added block-test page
    restyled hover for footer links

--- app/public/wp-content/themes/hoop/footer.php
+++ app/public/wp-content/themes/hoop/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,27 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hoop' ) ); ?>">
+<footer id="colophon" class="site-footer show-for-medium">
+	<div class="grid-container">
+		<div class="site-info grid-x">
+			<nav id="site-navigation" class="main-navigation cell small-12 medium-12">
+				<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'hoop'); ?></button> -->
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'hoop' ), 'WordPress' );
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-primary',
+						'menu_id'        => 'primary-menu',
+					)
+				);
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hoop' ), 'hoop', '<a href="http://www.millsdylan.com">Dylan Mills</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+			</nav><!-- #site-navigation -->
+		</div>
+	</div>
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
--- app/public/wp-content/themes/hoop/template-parts/content-page.php
+++ app/public/wp-content/themes/hoop/template-parts/content-page.php
@@ -30,25 +30,24 @@
 	</div><!-- .entry-content -->
 
 	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer  grid-x grid-margin-x grid-margin-y">
+	<!-- <footer class="entry-footer  grid-x grid-margin-x grid-margin-y">
 			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'hoop' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
+			// edit_post_link(
+			// 	sprintf(
+			// 		wp_kses(
+			// 			__( 'Edit <span class="screen-reader-text">%s</span>', 'hoop' ),
+			// 			array(
+			// 				'span' => array(
+			// 					'class' => array(),
+			// 				),
+			// 			)
+			// 		),
+			// 		wp_kses_post( get_the_title() )
+			// 	),
+			// 	'<span class="edit-link">',
+			// 	'</span>'
+			// );
 			?>
-		</footer><!-- .entry-footer -->
+		</footer> -->
 	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
